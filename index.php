e1e1723f7b7603e5e9c8b000551040a798b9796c
WalterCorsini <corsini.walter@gmail.com>
2024-06-01 15:07:09 +0200

    add dati in html

--- index.php
+++ index.php
@@ -11,13 +11,6 @@ $movie1 = new Movie('ciao mamma', $genre1);
 $movie1-> setData("21-11-1990");
 $movie1-> setVote(2);
 $movie1-> setDescription("descrizione film1");
-
-var_dump($movie1->getTitle());
-var_dump($movie1->getData());
-var_dump($movie1->getVote());
-var_dump($movie1->getDescription());
-var_dump($movie1->genre->getGenre());
-var_dump($movie1->genre->getAdultOnly());
 // /film 1
 
 // film 2
@@ -26,13 +19,36 @@ $movie2 = new Movie('ciao papa', $genre2);
 $movie2-> setData("21-11-1990");
 $movie2-> setVote(1);
 $movie2-> setDescription("descrizione film2");
-
-var_dump($movie2->getTitle());
-var_dump($movie2->getData());
-var_dump($movie2->getVote());
-var_dump($movie2->getDescription());
-var_dump($movie2->genre->getGenre());
-var_dump($movie2->genre->getAdultOnly());
 // /film 2
 
+$movie=[];
+$movie[]= $movie1;
+$movie[]= $movie2;
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+    <?php foreach($movie as $movie) {?>
+        <li><?php echo $movie->getTitle(); ?>
+            <ul>
+                <li><?php echo $movie->getData(); ?></li>
+                <li><?php echo $movie->getVote(); ?></li>
+                <li><?php echo $movie->getDescription(); ?></li>
+                <li><?php echo $movie->genre->getGenre(); ?></li>
+                <li><?php echo $movie->genre->getAdultOnly(); ?></li>
+            </ul>
+        </li>
+        
+<?php }?>
+    </ul>
+    <!-- echo $movie1->genre->getAdultOnly() -->
+</body>
+</html>
